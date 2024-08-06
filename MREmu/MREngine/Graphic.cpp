a7cf81d6c3fcc5c597c509b76b3bc714a6758dc7
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2024-08-06 13:11:48 +0300

    Some graphic fixes

--- MREmu/MREngine/Graphic.cpp
+++ MREmu/MREngine/Graphic.cpp
@@ -322,6 +322,16 @@ VMINT vm_graphic_get_bits_per_pixel(void) {
 	return 2;
 }
 
+MREngine::canvas_signature* find_canvas_signature(VMUINT8* buf) {
+	MREngine::canvas_signature* cs = (MREngine::canvas_signature*)buf;
+	if (memcmp(cs->magic, CANVAS_MAGIC, 9) == 0)
+		return cs;
+	cs = (MREngine::canvas_signature*)(buf - VM_CANVAS_DATA_OFFSET);
+	if (memcmp(cs->magic, CANVAS_MAGIC, 9) == 0)
+		return cs;
+	return 0;
+}
+
 VMINT vm_graphic_create_canvas(VMINT width, VMINT height) {
 	int image_size = width * height * 2;
 	void* canvas_buf = vm_malloc(VM_CANVAS_DATA_OFFSET + image_size);
@@ -362,8 +372,10 @@ void vm_graphic_release_canvas(VMINT hcanvas) {
 	std::lock_guard lock(get_current_app_graphic().canvases_list_mutex);
 
 	for (int i = 0; i < canvases_list.size(); ++i)
-		if (canvases_list[i].first == hcanvas_adr)
+		if (canvases_list[i].first == hcanvas_adr) {
 			canvases_list.erase(canvases_list.begin() + i);
+			break;
+		}
 
 	vm_free(hcanvas_adr);
 }
@@ -443,9 +455,9 @@ void vm_graphic_blt(VMBYTE* dst_disp_buf, VMINT x_dest, VMINT y_dest, VMBYTE* sr
 	if (dst_disp_buf == 0 || src_disp_buf == 0)
 		return;
 
-	MREngine::canvas_signature* cs_dst = (MREngine::canvas_signature*)(dst_disp_buf - VM_CANVAS_DATA_OFFSET);
-	MREngine::canvas_signature* cs_src = (MREngine::canvas_signature*)(src_disp_buf);
-	if (memcmp(cs_dst->magic, CANVAS_MAGIC, 9) || memcmp(cs_src->magic, CANVAS_MAGIC, 9))
+	MREngine::canvas_signature* cs_dst = find_canvas_signature(dst_disp_buf);
+	MREngine::canvas_signature* cs_src = find_canvas_signature(src_disp_buf);
+	if (!cs_dst || !cs_src)
 		return;
 	MREngine::canvas_frame_property* cfp_dst = (MREngine::canvas_frame_property*)(cs_dst + 1);
 	MREngine::canvas_frame_property* cfp_src = (MREngine::canvas_frame_property*)(cs_src + 1);
@@ -493,9 +505,9 @@ void vm_graphic_blt_ex(VMBYTE* dst_disp_buf, VMINT x_dest, VMINT y_dest, VMBYTE*
 	if (dst_disp_buf == 0 || src_disp_buf == 0)
 		return;
 
-	MREngine::canvas_signature* cs_dst = (MREngine::canvas_signature*)(dst_disp_buf - VM_CANVAS_DATA_OFFSET);
-	MREngine::canvas_signature* cs_src = (MREngine::canvas_signature*)(src_disp_buf);
-	if (memcmp(cs_dst->magic, CANVAS_MAGIC, 9) || memcmp(cs_src->magic, CANVAS_MAGIC, 9))
+	MREngine::canvas_signature* cs_dst = find_canvas_signature(dst_disp_buf);
+	MREngine::canvas_signature* cs_src = find_canvas_signature(src_disp_buf);
+	if (!cs_dst || !cs_src)
 		return;
 	MREngine::canvas_frame_property* cfp_dst = (MREngine::canvas_frame_property*)(cs_dst + 1);
 	MREngine::canvas_frame_property* cfp_src = (MREngine::canvas_frame_property*)(cs_src + 1);
@@ -545,11 +557,9 @@ void vm_graphic_rotate(VMBYTE* buf, VMINT x_des, VMINT y_des,
 	if (buf == 0 || src_buf == 0)
 		return;
 
-	//return; // wrong realization
-
-	MREngine::canvas_signature* cs_dst = (MREngine::canvas_signature*)(buf - VM_CANVAS_DATA_OFFSET);
-	MREngine::canvas_signature* cs_src = (MREngine::canvas_signature*)(src_buf);
-	if (memcmp(cs_dst->magic, CANVAS_MAGIC, 9) || memcmp(cs_src->magic, CANVAS_MAGIC, 9))
+	MREngine::canvas_signature* cs_dst = find_canvas_signature(buf);
+	MREngine::canvas_signature* cs_src = find_canvas_signature(src_buf);
+	if (!cs_dst || !cs_src)
 		return;
 	MREngine::canvas_frame_property* cfp_dst = (MREngine::canvas_frame_property*)(cs_dst + 1);
 	MREngine::canvas_frame_property* cfp_src = (MREngine::canvas_frame_property*)(cs_src + 1);
@@ -559,7 +569,7 @@ void vm_graphic_rotate(VMBYTE* buf, VMINT x_des, VMINT y_des,
 	int width = cfp_src->width;
 	int height = cfp_src->height;
 
-	if (degrees == VM_GDI_ROTATE_90 || degrees == VM_GDI_ROTATE_270)
+	if (degrees == VM_ROTATE_DEGREE_90 || degrees == VM_ROTATE_DEGREE_270)
 		std::swap(width, height);
 
 	int st_x = std::max(0, x_des);
@@ -588,13 +598,17 @@ void vm_graphic_rotate(VMBYTE* buf, VMINT x_des, VMINT y_des,
 			int im_x = sx - x_des;
 			int im_y = sy - y_des;
 
-			if (degrees == VM_ROTATE_DEGREE_90)
+			if (degrees == VM_ROTATE_DEGREE_90) {
 				std::swap(im_x, im_y);
-
-			if (degrees == VM_ROTATE_DEGREE_180)
+				im_y = width - im_y - 1;
+			}
+			else if (degrees == VM_ROTATE_DEGREE_270) {
+				std::swap(im_x, im_y);
+				im_x = height - im_x - 1;
+			}
+			else if (degrees == VM_ROTATE_DEGREE_180)
 				im_x = width - im_x - 1, im_y = height - im_y - 1;
-			else if (degrees == VM_ROTATE_DEGREE_270)
-				im_x = height - im_x - 1, im_y = width - im_y - 1;
+
 
 			unsigned short scr_color = buf16_src[im_y * cfp_src->width + im_x];
 			if (!flag || scr_color != trans_color)
@@ -607,9 +621,9 @@ void vm_graphic_mirror(VMBYTE* buf, VMINT x_des, VMINT y_des, VMBYTE* src_buf, V
 	if (buf == 0 || src_buf == 0)
 		return;
 
-	MREngine::canvas_signature* cs_dst = (MREngine::canvas_signature*)(buf - VM_CANVAS_DATA_OFFSET);
-	MREngine::canvas_signature* cs_src = (MREngine::canvas_signature*)(src_buf);
-	if (memcmp(cs_dst->magic, CANVAS_MAGIC, 9) || memcmp(cs_src->magic, CANVAS_MAGIC, 9))
+	MREngine::canvas_signature* cs_dst = find_canvas_signature(buf);
+	MREngine::canvas_signature* cs_src = find_canvas_signature(src_buf);
+	if (!cs_dst || !cs_src)
 		return;
 	MREngine::canvas_frame_property* cfp_dst = (MREngine::canvas_frame_property*)(cs_dst + 1);
 	MREngine::canvas_frame_property* cfp_src = (MREngine::canvas_frame_property*)(cs_src + 1);
@@ -657,8 +671,8 @@ void vm_graphic_mirror(VMBYTE* buf, VMINT x_des, VMINT y_des, VMBYTE* src_buf, V
 }
 
 void vm_graphic_set_pixel(VMUINT8* buf, VMINT x, VMINT y, VMUINT16 color) {
-	MREngine::canvas_signature* cs_dst = (MREngine::canvas_signature*)(buf - VM_CANVAS_DATA_OFFSET);
-	if (memcmp(cs_dst->magic, CANVAS_MAGIC, 9))
+	MREngine::canvas_signature* cs_dst = find_canvas_signature(buf);
+	if (!cs_dst)
 		return;
 	MREngine::canvas_frame_property* cfp_dst = (MREngine::canvas_frame_property*)(cs_dst + 1);
 	unsigned short* buf16_dst = (unsigned short*)(cfp_dst + 1);
@@ -687,8 +701,8 @@ void vm_graphic_set_pixel(VMUINT8* buf, VMINT x, VMINT y, VMUINT16 color) {
 }
 
 void vm_graphic_line(VMUINT8* buf, VMINT x0, VMINT y0, VMINT x1, VMINT y1, VMUINT16 color) {
-	MREngine::canvas_signature* cs_dst = (MREngine::canvas_signature*)(buf - VM_CANVAS_DATA_OFFSET);
-	if (memcmp(cs_dst->magic, CANVAS_MAGIC, 9))
+	MREngine::canvas_signature* cs_dst = find_canvas_signature(buf);
+	if (!cs_dst)
 		return;
 	MREngine::canvas_frame_property* cfp_dst = (MREngine::canvas_frame_property*)(cs_dst + 1);
 	unsigned short* buf16_dst = (unsigned short*)(cfp_dst + 1);
@@ -727,7 +741,8 @@ void vm_graphic_line(VMUINT8* buf, VMINT x0, VMINT y0, VMINT x1, VMINT y1, VMUIN
 
 			buf16_dst[y * cfp_dst->width + x] = color;
 		}
-	}else {
+	}
+	else {
 		if (y0 > y1) {
 			std::swap(x0, x1);
 			std::swap(y0, y1);
@@ -745,10 +760,9 @@ void vm_graphic_line(VMUINT8* buf, VMINT x0, VMINT y0, VMINT x1, VMINT y1, VMUIN
 	}
 }
 
-
 void vm_graphic_fill_rect(VMUINT8* buf, VMINT x, VMINT y, VMINT width, VMINT height, VMUINT16 line_color, VMUINT16 back_color) {
-	MREngine::canvas_signature* cs_dst = (MREngine::canvas_signature*)(buf - VM_CANVAS_DATA_OFFSET);
-	if (memcmp(cs_dst->magic, CANVAS_MAGIC, 9))
+	MREngine::canvas_signature* cs_dst = find_canvas_signature(buf);
+	if (!cs_dst)
 		return;
 	MREngine::canvas_frame_property* cfp_dst = (MREngine::canvas_frame_property*)(cs_dst + 1);
 	unsigned short* buf16_dst = (unsigned short*)(cfp_dst + 1);
@@ -790,7 +804,6 @@ void vm_graphic_fill_rect_ex(VMINT handle, VMINT  x, VMINT  y, VMINT  width, VMI
 	unsigned short c = get_current_app_graphic().global_color.vm_color_565;
 
 	vm_graphic_fill_rect((VMUINT8*)layer.buf, x, y, width, height, c, c);
-;
 }
 
 bool is_point_in_path(int x, int y, vm_graphic_point* point, VMINT npoints) {
