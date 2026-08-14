b7c399cf426b9628c87c3b10e485c5b0fdafad8b
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2026-08-14 15:38:03 +0300

    Fixed status bar in full size

--- Android/app/src/main/java/com/ximikboda/mremu/MainActivity.kt
+++ Android/app/src/main/java/com/ximikboda/mremu/MainActivity.kt
@@ -14,6 +14,9 @@ import android.content.Context
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.os.VibratorManager
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import java.io.File
 import java.io.FileOutputStream
 
@@ -35,12 +38,21 @@ class MainActivity : NativeActivity() {
 
         checkAndRequestStoragePermission()
 
+        hideSystemUI()
+
         handleIntent(intent)
     }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         handleIntent(intent)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUI()
+        }
     }
 
     private fun checkAndRequestStoragePermission() {
@@ -98,6 +110,26 @@ class MainActivity : NativeActivity() {
                 Log.e("MREmu", "User denied WRITE_EXTERNAL_STORAGE")
                 notifyPermissionState(false)
             }
+        }
+    }
+
+    private fun hideSystemUI() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.let { controller ->
+                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    )
         }
     }
 
